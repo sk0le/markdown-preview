70a81adf2234479201ae2e6666f310fa504a786b
Amel Islamovic <aislqmovic@gmail.com>
2020-06-02 18:33:48 +0200

    Fixed bug with public path

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const app = express();
 
-const publicPath = paht.join(__dirname, "..", "public");
+const publicPath = path.join(__dirname, "..", "public");
 app.use(express.static(publicPath));
 
 app.get("*", (req, res) => {
