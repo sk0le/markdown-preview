43f17000c95af0dddc63c61932d19861a34687f5
Amel Islamovic <aislqmovic@gmail.com>
2020-06-02 18:30:06 +0200

    Added Express for back-end

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const publicPath = paht.join(__dirname, "..", "public");
+app.use(express.static(publicPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
